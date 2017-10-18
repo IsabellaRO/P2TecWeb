47df3b5f979e588cdaf3fa2e55b7a902a2d44cc4
rachelbottino <rachelpbm@gmail.com>
2017-10-18 02:51:57 -0200

    Inicio controller posts

--- Projeto2/src/mvc/controller/PostsController.java
+++ Projeto2/src/mvc/controller/PostsController.java
@@ -2,7 +2,31 @@ package mvc.controller;
 
 import mvc.model.Posts;
 import mvc.model.PostsDAO;
+import mvc.model.Tarefa;
+import mvc.model.TarefasDAO;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 public class PostsController {
+	@RequestMapping("post")
+	public String postagem() {
+		return "post";
+	}
+	
+	@RequestMapping(value ="criaQuote", method = RequestMethod.POST)
+	public String criaPost(@Valid Posts post, BindingResult result) {
+		if(result.hasErrors()){
+			return "meuperfil";
+		}
+		PostsDAO dao = new PostsDAO();
+		dao.adiciona(post);
+		return "meuperfil";
+	 }
 
 }
